7803cfd8678bf169dc51b58faf1ad2c3afe6531b
Peter Kan <petervsjim@gmail.com>
2021-06-14 20:39:02 +0800

    6/14 20:39

--- slicesumall/SumAllTails.go
+++ slicesumall/SumAllTails.go
@@ -4,16 +4,17 @@ func SumAllTails(s1, s2 []int) (SumAll []int) {
 
 	// var SumAll []int
 
-	sums := 0
+	sums1 := 0
+	sums2 := 0
+
 	for _, number := range s1[1:] {
-		sums = sums + number
+		sums1 = sums1 + number
 	}
-	SumAll = append(SumAll, sums)
-	sums = 0
 	for _, number := range s2[1:] {
-		sums = sums + number
+		sums2 = sums2 + number
 	}
-	SumAll = append(SumAll, sums)
+
+	SumAll = append(SumAll, sums1, sums2)
 
 	return SumAll
 }
--- slicesumall/SumAllTails_test.go
+++ slicesumall/SumAllTails_test.go
@@ -4,25 +4,23 @@ import "testing"
 
 func TestSumAllTails(t *testing.T) {
 
-	t.Run("check if two same work", func(t *testing.T) {
-		got := SumAllTails([]int{1, 2}, []int{3, 4})
-		want := []int{2, 4}
-
+	checkSums := func(t *testing.T, got, want []int) {
 		for i := 0; i < len(got); i++ {
 			if got[0] != want[0] {
 				t.Errorf("got '%v' but want '%v'", got, want)
 			}
 		}
+	}
+
+	t.Run("check if two same work", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{3, 4})
+		want := []int{2, 4}
+		checkSums(t, got, want)
 	})
 
 	t.Run("check if different length work", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2, 3}, []int{3, 4})
 		want := []int{5, 4}
-
-		for i := 0; i < len(got); i++ {
-			if got[0] != want[0] {
-				t.Errorf("got '%v' but want '%v'", got, want)
-			}
-		}
+		checkSums(t, got, want)
 	})
 }
