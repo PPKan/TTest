31e0cbbc591ad084638dc89f3d6d2c7f987dd806
Peter Kan <petervsjim@gmail.com>
2021-06-14 17:05:15 +0800

    6/14 17:05

--- slice/slice_test.go
+++ slice/slice_test.go
@@ -4,13 +4,25 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	numbers := [...]int{1, 2, 3, 4, 5}
+	// t.Run("collection of 5 numbers", func(t *testing.T) {
+	// 	numbers := []int{1, 2, 3, 4, 5}
 
-	got := sum(numbers)
-	want := 15
+	// 	got := sum(numbers)
+	// 	want := 15
 
-	if got != want {
-		t.Errorf("from '%v' we have got '%d' but wanted '%d'", numbers, got, want)
-	}
+	// 	if got != want {
+	// 		t.Errorf("from '%v' we have got '%d' but wanted '%d'", numbers, got, want)
+	// 	}
+	// })
 
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
